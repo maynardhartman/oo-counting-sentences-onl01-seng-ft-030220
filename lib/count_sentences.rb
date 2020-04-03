951988e6b9e9e8358a28bbdfc4f658e34c15974f
Maynard Hartman <mjhartman@brighthouse.com>
2020-04-03 00:02:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".") ? true : false
+  
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    delimiter = "/[,.!?/g]"
+    count = self.split(delimiter)
+    return(count.length)
   end
 end
